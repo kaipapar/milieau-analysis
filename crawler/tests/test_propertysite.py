58902e36332c71f42bc15f03db18484980e4b9f8
Karri Korsu <kkorsu@gmail.com>
2025-12-09 17:21:22 +0200

    Wrote preliminary tests for remax and listing, further defined remax and listing. Tests run successfully

--- crawler/tests/test_propertysite.py
+++ crawler/tests/test_propertysite.py
@@ -16,21 +16,45 @@ class TestRemax:
      check that url and next page button are correct
 
        """
-    def test_appending_is_implemented(self):
-        """ Checks that list append is implemented """
+    
+    @pytest.fixture
+    def remax(self):
         remax = Remax()
-        
-        with pytest.raises(NotImplementedError) as excinfo:
-                remax.listing_append(listing=remax.Listing())  # the function ignores its args parameter anyway   
+        yield remax
+
+    @pytest.fixture
+    def populate_remax(self,remax):
+        # create multiple Listings, insert them to listing_list
+        list = [remax.Listing(id=123, attr_dict={'address':'Tuomiokirkontori 1'}), 
+                remax.Listing(id=456, attr_dict={'address':'Linnanaukio 2b'})
+                ]
+        remax.listing_list = list
+        yield remax
+
+    def test_appending_is_implemented(self, remax):
+        """ Checks that list append is implemented """
+        remax.listing_append(listing=remax.Listing(id=1, attr_dict={'address':'kumputie'}))
+
+    def test_listing_list_population(self, remax, populate_remax):
+        """ expectation = [remax.Listing(id=123, attr_dict={'address':'Tuomiokirkontori 1'}), 
+                remax.Listing(id=456, attr_dict={'address':'Linnanaukio 2b'})
+                ]
+        expectation """
+        assert populate_remax.listing_list != None
+
 
     class TestListing:
-        def test_html_formatting(self):
+        @pytest.fixture
+        def listing(self):
+            new_listing = Remax.Listing(id=1, attr_dict={'address':'kumputie 3'})
+            yield new_listing
+
+        def test_html_formatting(self, listing):
             html_tuple = ("<div>","</div>")
             attr = "Pinta-ala"
             expectation = f"{html_tuple[0]}{attr}{html_tuple[1]}"
-            object = Remax.Listing()
-            object.attr_html = html_tuple
-            object.attr_keys = ["something", attr]
+            listing.attr_html = html_tuple
+            listing.attr_keys = ["something", attr]
 
-            result = object.attr_key_html(1)
+            result = listing.attr_key_html(0)
             assert result == expectation
