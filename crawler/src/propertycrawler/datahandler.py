d003b9892bb2779acc60d72a8859a9682afbece3
Karri Korsu <kkorsu@gmail.com>
2026-02-16 19:57:05 +0200

    finish get_file

--- crawler/src/propertycrawler/datahandler.py
+++ crawler/src/propertycrawler/datahandler.py
@@ -31,17 +31,20 @@ class IO:
                   d.append(line)
         return d
     
-    """ def get_html_fp(path: pathlib.Path):
+    def get_file(path: str):
+        path = pathlib.Path(path) #TODO refactoring str literal paths to Path type...
         try:
             path.exists()
         except FileNotFoundError:
             return
-        with open()
-                   """
+        with open(path) as file:
+            return file
+                  
 
             
     
     def is_empty(filepath) -> bool:
+        """ returns True if file is "empty" or doesn't exist. In use with crawler methods. """
         threshold = 100 #in bytes, it seems a empty file returned by wget is 2 bytes and a full one ~13k bytes
         if (os.path.exists(filepath)):
             if (os.path.getsize(filepath) > threshold):
